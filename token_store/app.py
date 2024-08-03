951a537853c78e2e69e897c057f9df8972a500db
canaparro <marcelo.canaparro@pagefreezer.com>
2024-08-03 15:58:14 +0100

    failing due to test loop conflicts in asyncpg

--- token_store/app.py
+++ token_store/app.py
@@ -5,6 +5,7 @@ import uvicorn
 from fastapi import FastAPI
 from tortoise.contrib.fastapi import RegisterTortoise
 
+from token_store.config import DATABASE_USERNAME, DATABASE_PASSWORD
 from token_store.web import exception_handlers
 from token_store.routers import tokens
 
@@ -14,7 +15,7 @@ async def lifespan(fastapi_app: FastAPI) -> AsyncGenerator[None, None]:
     # app startup
     async with RegisterTortoise(
             fastapi_app,
-            db_url="sqlite://db.sqlite3",
+            db_url=f"postgres://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@localhost:5432/postgres",
             modules={"models": ["token_store.persistence.models"]},
             generate_schemas=True,
             add_exception_handlers=True,
@@ -29,6 +30,7 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_exception_handler(Exception, exception_handlers.http_exception_handler)
 app.include_router(tokens.router)
+
 
 if __name__ == "__main__":
     uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
