32a2ed9b9ae81d0b73f619cd11e1489748f5da49
canaparro <marcelo.canaparro@pagefreezer.com>
2024-08-14 18:37:27 +0100

    test

---
+++ token_store/service/module1.py
@@ -0,0 +1,9 @@
+# math_operations.py
+
+def add(x, y):
+    """Returns the sum of x and y."""
+    return x + y
+
+def subtract(x, y):
+    """Returns the difference between x and y."""
+    return x - y
