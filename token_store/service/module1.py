1a0fb5991614278610d7cf87dfaf59411a336a55
canaparro <marcelo.canaparro@pagefreezer.com>
2024-08-15 11:37:31 +0100

    test3

--- token_store/service/module1.py
+++ token_store/service/module1.py
@@ -4,6 +4,12 @@ def add(x, y):
     """Returns the sum of x and y."""
     return x + y
 
+def multiply(x, y):
+    """Returns the multiplication between x and y."""
+    return x * y
 def subtract(x, y):
     """Returns the difference between x and y."""
     return x - y
+def divide(x, y):
+    """Returns the division between x and y."""
+    return x / y
